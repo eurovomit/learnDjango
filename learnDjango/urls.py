71d50c3e1f35caa8f73bd82a7d0d3e12e9bbdc76
eurovomit <eurovomit@gmail.com>
2025-01-08 23:19:48 +0200

    3 commit

--- learnDjango/urls.py
+++ learnDjango/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import homepage
+from learnDjango import views
 
-
-urlpatterns = [path("", homepage, name="home")]
+urlpatterns = [path("", views.index),
+               path("about/", views.about, kwargs={'name': 'Tom', 'age': 20}),
+               path("contact/", views.contact),
+               path('user/<str:name>/<int:age>', views.user),
+               path('user/<str:name>/', views.user),
+               path('user/', views.user),]
--- learnDjango/views.py
+++ learnDjango/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 
 
-def homepage(request):
-    return HttpResponse("Hello, world")
+def index(request):
+    return HttpResponse('<h2>Главная</h2>')
+
+def about(request, name, age):
+    return HttpResponse(f'<h2>О пользователе</h2><p>Имя {name}</p><p>Возраст {age}</p>')
+
+def contact(request):
+    return HttpResponse('<h2>Контакты</h2>')
+
+def user(request, name='undefined', age=20):
+    return HttpResponse(f'<h2>Имя: {name} Возраст: {age}</h2>')
