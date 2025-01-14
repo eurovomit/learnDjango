78aabce8874a6f247112d506390d6aed5ae6d57e
eurovomit <eurovomit@gmail.com>
2025-01-14 20:13:30 +0200

    4 commit

--- learnDjango/urls.py
+++ learnDjango/urls.py
@@ -1,9 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from learnDjango import views
 
+
 urlpatterns = [path("", views.index),
-               path("about/", views.about, kwargs={'name': 'Tom', 'age': 20}),
-               path("contact/", views.contact),
-               path('user/<str:name>/<int:age>', views.user),
-               path('user/<str:name>/', views.user),
-               path('user/', views.user),]
+               path("user/", views.user),]
--- learnDjango/views.py
+++ learnDjango/views.py
@@ -2,13 +2,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h2>Главная</h2>')
+    return HttpResponse('Главная страница')
 
-def about(request, name, age):
-    return HttpResponse(f'<h2>О пользователе</h2><p>Имя {name}</p><p>Возраст {age}</p>')
-
-def contact(request):
-    return HttpResponse('<h2>Контакты</h2>')
-
-def user(request, name='undefined', age=20):
-    return HttpResponse(f'<h2>Имя: {name} Возраст: {age}</h2>')
+def user(request):
+    age = request.GET.get('age', 0)
+    name = request.GET.get('name', 'undefined')
+    return HttpResponse(f'Имя: {name} Возраст: {age}')
