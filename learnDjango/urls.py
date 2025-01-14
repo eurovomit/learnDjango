9e80b9a153758c2459e6ffa5bab797e02480c3b4
eurovomit <eurovomit@gmail.com>
2025-01-14 20:25:56 +0200

    5 commit

--- learnDjango/urls.py
+++ learnDjango/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from learnDjango import views
 
 
-urlpatterns = [path("", views.index),
-               path("user/", views.user),]
+urlpatterns = [path("index/<int:id>", views.index),
+               path("about/", views.about),
+               path("contact/", views.contact),
+               path("details/", views.details),]
--- learnDjango/views.py
+++ learnDjango/views.py
@@ -1,10 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseNotFound
 
 
-def index(request):
-    return HttpResponse('Главная страница')
+def index(request, id):
+    people = [None, "Bob", "Sam", "Tom"]
+    if id in range(1, len(people)):
+        return HttpResponse(people[id])
+    else:
+        return HttpResponseNotFound("Not found")
 
-def user(request):
-    age = request.GET.get('age', 0)
-    name = request.GET.get('name', 'undefined')
-    return HttpResponse(f'Имя: {name} Возраст: {age}')
+def about(request):
+    return HttpResponse('about')
+
+def contact(request):
+    return HttpResponseRedirect('/about')
+
+def details(request):
+    return HttpResponsePermanentRedirect('/')
+
